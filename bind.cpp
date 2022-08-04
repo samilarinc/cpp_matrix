e0ba920b7e0b909e8f5b15159ec3359f8ecba81e
samilarinc <msarinc@hotmail.com>
2022-08-04 05:06:08 +0200

    temp in process

--- bind.cpp
+++ bind.cpp
@@ -6,35 +6,41 @@
 
 namespace py = pybind11;
 
+template<>
+Operational1D<double>;
+
+template<>
+Operational1D<int>;
+
 PYBIND11_MODULE(MatrixOps, m) {
     py::class_<Operational1D>(m, "Operational1D")
         .def(py::init< vector<vector<double>> >())
-        .def("__add__", &Operational1D::addMatrix)
-        .def("__sub__", &Operational1D::substractMatrix)
-        .def("__mul__", &Operational1D::multiplyMatrix)
-        .def("__truediv__", &Operational1D::divideMatrix)
-        .def("__floordiv__", &Operational1D::intdivideMatrix)
-        .def("__matmul__", &Operational1D::atMatrix)
-        .def("__repr__", &Operational1D::printMatrix)
-        .def("__getitem__", py::overload_cast<pair<int, int>>(&Operational1D::at))
-        .def("__getitem__", py::overload_cast<int>(&Operational1D::at))
-        .def("__setitem__", py::overload_cast<pair<int, int>, double>(&Operational1D::set))
-        .def("__setitem__", py::overload_cast<int, vector<double>>(&Operational1D::set))
-        .def("__add__", &Operational1D::addConstant)
-        .def("__sub__", &Operational1D::subtractConstant)
-        .def("__mul__", &Operational1D::multiplyConstant)
-        .def("__truediv__", &Operational1D::divideConstant)
-        .def("__floordiv__", &Operational1D::intdivideConstant)
-        .def("__len__", &Operational1D::length)
-        .def("T", &Operational1D::T)
-        .def("transpose", &Operational1D::T)
-        .def("det", &Operational1D::det)
-        .def("inv", &Operational1D::inverse)
-        .def("adjoint", &Operational1D::adjoint)
-        .def("cofactor", &Operational1D::cofactor)
-        .def("copy", &Operational1D::copy)
-        .def_readonly("height", &Operational1D::height)
-        .def_readonly("width", &Operational1D::width)
-        .def_readonly("shape", &Operational1D::shape)
+        .def("__add__", &Operational1D<double>::addMatrix)
+        .def("__sub__", &Operational1D<double>::substractMatrix)
+        .def("__mul__", &Operational1D<double>::multiplyMatrix)
+        .def("__truediv__", &Operational1D<double>::divideMatrix)
+        .def("__floordiv__", &Operational1D<double>::intdivideMatrix)
+        .def("__matmul__", &Operational1D<double>::atMatrix)
+        .def("__repr__", &Operational1D<double>::printMatrix)
+        .def("__getitem__", py::overload_cast<pair<int, int>>(&Operational1D<double>::at))
+        .def("__getitem__", py::overload_cast<int>(&Operational1D<double>::at))
+        .def("__setitem__", py::overload_cast<pair<int, int>, double>(&Operational1D<double>::set))
+        .def("__setitem__", py::overload_cast<int, vector<double>>(&Operational1D<double>::set))
+        .def("__add__", &Operational1D<double>::addConstant)
+        .def("__sub__", &Operational1D<double>::subtractConstant)
+        .def("__mul__", &Operational1D<double>::multiplyConstant)
+        .def("__truediv__", &Operational1D<double>::divideConstant)
+        .def("__floordiv__", &Operational1D<double>::intdivideConstant)
+        .def("__len__", &Operational1D<double>::length)
+        .def("T", &Operational1D<double>::T)
+        .def("transpose", &Operational1D<double>::T)
+        .def("det", &Operational1D<double>::det)
+        .def("inv", &Operational1D<double>::inverse)
+        .def("adjoint", &Operational1D<double>::adjoint)
+        .def("cofactor", &Operational1D<double>::cofactor)
+        .def("copy", &Operational1D<double>::copy)
+        .def_readonly("height", &Operational1D<double>::height)
+        .def_readonly("width", &Operational1D<double>::width)
+        .def_readonly("shape", &Operational1D<double>::shape)
         ;
 }
